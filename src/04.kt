23d7953022908bf3d1c7aa08ec193886c384905a
LonathG <lk.gcm29@gmail.com>
2026-07-24 19:08:06 +0530

    Initial commit

---
+++ src/04.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+     val names = arrayOf("Lonath", "Sakith", "Rusheni", "Uvindu", "Yasith")
+
+     for ((index, name) in names.withIndex()) {
+         println("$name")
+     }
+
+}
